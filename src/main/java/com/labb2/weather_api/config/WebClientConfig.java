6d167bd5eb352999a08011ab9bc2b266a00cade3
BobbyBrogrammer <robin.lindholm111@gmail.com>
2026-05-30 15:16:38 +0200

    (Step 2&3 - V22) Added: WebClientConfig with WebClient.Builder Bean

---
+++ src/main/java/com/labb2/weather_api/config/WebClientConfig.java
@@ -0,0 +1,18 @@
+package com.labb2.weather_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+/*
+Konfigurationsklass som registrerar WebClient.Builder som en Spring Bean
+Som används av WeatherClient och ActivityClient för att bygga sina egna WebClient instanser
+ */
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+}
